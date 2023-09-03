62de800bbc89f5942c0953fe8f3fd2558d4da4a8
Karl Page <karlp2485@hotmail.com>
2023-09-03 16:41:20 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Gui
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.testView1 = new Gui.TestView();
+      this.SuspendLayout();
+      // 
+      // testView1
+      // 
+      this.testView1.BackColor = System.Drawing.SystemColors.Window;
+      this.testView1.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.testView1.Location = new System.Drawing.Point(0, 0);
+      this.testView1.Name = "testView1";
+      this.testView1.Size = new System.Drawing.Size(360, 341);
+      this.testView1.TabIndex = 0;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(360, 341);
+      this.Controls.Add(this.testView1);
+      this.Name = "Form1";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private TestView testView1;
+  }
+}
+
---
+++ TestView.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Gui
+{
+  public partial class TestView : UserControl
+  {
+    private IEnumerable<string> _rowData = new string[0];
+    public TestView()
+    {
+      InitializeComponent();
+    }
+
+    public static IEnumerable<string> GenerateRowData(int count)
+    {
+      for (int i = 0; i < count; i++)
+        yield return $"Row {i}";
+    }
+
+    protected override void OnLoad(EventArgs e)
+    {
+      _rowData = GenerateRowData(30);
+    }
+
+    protected override void OnPaint(PaintEventArgs e)
+    {
+      Graphics g = e.Graphics;
+      Rectangle rcRow = ClientRectangle;
+      int rowHeight = Font.Height;
+      rcRow.Height = rowHeight;
+
+      foreach (string rowText in _rowData)
+      {
+        g.DrawString(rowText, Font, Brushes.Black, rcRow);
+        rcRow.Y += rowHeight;
+      }
+    }
+  }
+}
